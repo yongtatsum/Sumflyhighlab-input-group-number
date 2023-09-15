8054907072e71946bc68d6595bf55c535623e825
Sum <tech@eatcosys.com>
2023-09-15 15:40:15 +0800

    support for nova 4

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,6 +1,18 @@
 let mix = require('laravel-mix')
 
+require('./nova.mix')
+
 mix
   .setPublicPath('dist')
   .js('resources/js/field.js', 'js')
-  .sass('resources/sass/field.scss', 'css')
+  .vue({ version: 3 })
+  .webpackConfig({
+    // stats: { children: true },
+    externals: {
+      vue: 'Vue',
+      'laravel-nova': 'LaravelNova',
+    },
+    output: {
+      uniqueName: 'sumflyhighlab/text-combine-number',
+    },
+  })
